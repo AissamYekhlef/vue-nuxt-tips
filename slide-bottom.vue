ee98ca95873f01db049282672372e9e54571d7d9
Aissam Yekhlef <aissamyekhlef@gmail.com>
2026-09-22 10:28:17 +0100

    add footer current page / total and next, prev page navigation

---
+++ slide-bottom.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer>
+    <div class="fixed bottom-2 left-10 text-xs opacity-40 tabular-nums z-10">
+        <button class="icon-btn" title="Next" @click="prev">
+            <div class="i-carbon:arrow-left" />
+        </button>
+    </div>
+    <div class="fixed bottom-2 right-50% text-xs opacity-40 tabular-nums z-10">
+        {{ currentPage }} / {{ total}}
+    </div>
+    <div class="fixed bottom-2 right-10 text-xs opacity-40 tabular-nums z-10">
+        <button class="icon-btn" title="Next" @click="next">
+            <div class="i-carbon:arrow-right" />
+        </button>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+import {  useNav } from '@slidev/client'
+
+const { currentPage, total, next, prev } = useNav()
+</script>
