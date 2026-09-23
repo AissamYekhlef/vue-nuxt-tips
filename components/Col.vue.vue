d10068359266b1ee74c7f4af763e6eb6f7cac1dc
Aissam Yekhlef <aissamyekhlef@gmail.com>
2026-09-23 10:21:25 +0100

    feat: added Row and Col components to split slide

---
+++ components/Col.vue.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  cols?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  cols: 1,
+})
+</script>
+
+<template>
+  <div
+    class="col"
+    :style="{ flex: props.cols }"
+  >
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.col {
+  min-width: 0;
+}
+</style>
